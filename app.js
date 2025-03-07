bbd22f8e42fbf84493231d6f229bb957356c13f3
Aliaa <aliaaalihesh@gmail.com>
2025-03-07 20:46:17 +0200

    GeminiChat

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import myConnection from "./Database/dbconnection.js";
 import customerRoute from './Modules/User/user.route.js';
@@ -5,19 +7,21 @@ import ProductRoute from './Modules/Product/product.route.js';
 import path from "path";
 import CartRoute from './Modules/Cart/cart.route.js';
 import { fileURLToPath } from "url";
+import ChatRoute from "./Modules/routes/geminiRoutes.route.js";
 
 const app = express();
 app.use(express.json());
 
 app.use('/customer', customerRoute);
 app.use('/product', ProductRoute);
-app.use('/cart',CartRoute)
+app.use('/cart',CartRoute);
+app.use("/gemini", ChatRoute);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 myConnection
 
-app.listen(5000, function(){
+app.listen(3000, function(){
     console.log("Server is running on port 3030");
 });
