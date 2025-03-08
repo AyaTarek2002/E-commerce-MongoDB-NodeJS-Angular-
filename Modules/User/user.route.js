7559d727c6350ba67038cb50cb77056d08e45633
salma457 <you@example.com>
2025-03-08 16:45:18 +0200

    signup

--- Modules/User/user.route.js
+++ Modules/User/user.route.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import passport from "passport";
+
 import { signUp ,login, verifyEmail } from '../User/user.controller.js';
 import { EmailCheck } from '../../MiddleWare/EmailChecked.js';
 import { validateUser } from "../../MiddleWare/validationUser.js";
@@ -8,5 +10,29 @@ const customerRoute = express.Router();
 customerRoute.post('/signUp',validateUser,EmailCheck, signUp);
 customerRoute.post('/login', login);
 customerRoute.get("/verify/:email", verifyEmail)
+
+// تسجيل الدخول عبر Google
+customerRoute.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+
+// مسار إعادة التوجيه بعد نجاح تسجيل الدخول من Google
+customerRoute.get(
+    "/auth/google/callback",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    (req, res) => {
+        res.json({ message: `Welcome ${req.user.name}`, token: req.user._id });
+    }
+);
+
+// تسجيل الدخول عبر Facebook
+customerRoute.get("/auth/facebook", passport.authenticate("facebook", { scope: ["email"] }));
+
+// مسار إعادة التوجيه بعد نجاح تسجيل الدخول من Facebook
+customerRoute.get(
+    "/auth/facebook/callback",
+    passport.authenticate("facebook", { failureRedirect: "/login" }),
+    (req, res) => {
+        res.json({ message: `Welcome ${req.user.name}`, token: req.user._id });
+    }
+);
 
 export default customerRoute;
--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 import express from "express";
+import session from "express-session";
+import passport from "passport";
+import dotenv from "dotenv";
+
+dotenv.config(); 
+
 import myConnection from "./Database/dbconnection.js";
 import customerRoute from './Modules/User/user.route.js';
 import ProductRoute from './Modules/Product/product.route.js';
 import path from "path";
 import { fileURLToPath } from "url";
+import "./MiddleWare/passport.js"; // استدعاء ملف المصادقة
 
 const app = express();
 app.use(express.json());
 
-app.use('/customer', customerRoute);
+
+// إعداد جلسة المستخدم
+app.use(
+    session({
+        secret: "mySecretKey",
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use( customerRoute);
 app.use('/product', ProductRoute);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use("/images", express.static(path.join(__dirname, "images")));
 
-myConnection
+myConnection;
 
 app.listen(3030, function(){
     console.log("Server is running on port 3000");
